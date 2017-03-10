740b195f5c7179d034b4d61d662264a9012e816c
Alexander Komarov <ak@akxcv.com>
2017-03-10 13:23:35 +0300

    Add a test for reset() on transition end

--- test/AnimatedCounterDigit.test.js
+++ test/AnimatedCounterDigit.test.js
@@ -1,5 +1,5 @@
-import { AnimatedCounterDigitBuilder as Builder } from './support/builders'
 import { shallowToJson } from 'enzyme-to-json'
+import { AnimatedCounterDigitBuilder as Builder } from './support/builders'
 
 it('matches snapshot', function () {
   const component = Builder.shallow()
@@ -10,4 +10,12 @@ it('matches snapshot', function () {
 it('limits the value to maxValue', function () {
   const component = Builder.shallow({ maxValue: 5 })
   expect(component.find('.rollex-digit-lane-label').length).toBe(7)
+})
+
+test('reset() is being called on transition end', function () {
+  const component = Builder.mount()
+  component.instance().reset = jest.genMockFunction()
+  component.instance().forceUpdate() // required for the mock to work
+  component.find('.rollex-digit').simulate('transitionEnd')
+  expect(component.instance().reset.mock.calls.length).toBe(1)
 })
