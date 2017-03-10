bd5c8353ea5a14e77cba810ed5216cf79c81f7ee
Alexander Komarov <ak@akxcv.com>
2017-03-10 11:47:28 +0300

    Set up GitHub Pages; minor maxValue calculation fix

--- src/docs.js
+++ src/docs.js
@@ -9,15 +9,18 @@ ReactDOM.render(
     />
     <Counter
       seconds={24891235}
+      minDigits={3}
     />
     <Counter
       seconds={243523}
     />
     <Counter
       seconds={53423}
+      radix={2}
     />
     <Counter
       seconds={10}
+      interval={2000}
     />
     <Counter
       seconds={1000000}
@@ -27,10 +30,16 @@ ReactDOM.render(
       seconds={3128741412}
       easingFunction='ease-in-out'
       easingDuration={800}
+      maxDigits={2}
     />
     <Counter
       seconds={4132749}
       easingFunction='ease-out'
+      radix={12}
+      digitMap={{
+        'a': 'X',
+        'b': 'E'
+      }}
     />
     <Counter
       seconds={10}
@@ -39,6 +48,7 @@ ReactDOM.render(
     <Counter
       seconds={3128414}
       easingFunction='cubic-bezier(0.6, -0.28, 0.735, 0.045)'
+      radix={16}
     />
   </div>,
   document.getElementById('app')
