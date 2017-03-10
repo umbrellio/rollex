12b0c4502e552e5c4c92658cd70ef269d87573af
Alexander Komarov <ak@akxcv.com>
2017-03-10 12:50:27 +0300

    Test builders

--- test/AnimatedCounterDigit.test.js
+++ test/AnimatedCounterDigit.test.js
@@ -1,37 +1,13 @@
-import React from 'react'
-import AnimatedCounterDigit from '../src/AnimatedCounterDigit'
-import { shallow } from 'enzyme'
+import { AnimatedCounterDigitBuilder as Builder } from './support/builders'
 import { shallowToJson } from 'enzyme-to-json'
 
 it('matches snapshot', function () {
-  const component = shallow(
-    <AnimatedCounterDigit
-      digit='0'
-      height={18}
-      radix={10}
-      digitMap={{}}
-      digitWrapper={(digit) => <div>{digit}</div>}
-      maxValue={9}
-      easingFunction='ease-in'
-      easingDuration={300}
-    />
-  )
+  const component = Builder.shallow()
   const tree = shallowToJson(component)
   expect(tree).toMatchSnapshot()
 })
 
 it('limits the value to maxValue', function () {
-  const component = shallow(
-    <AnimatedCounterDigit
-      digit='0'
-      height={18}
-      radix={10}
-      digitMap={{}}
-      digitWrapper={(digit) => digit}
-      maxValue={5}
-      easingFunction='ease-in'
-      easingDuration={300}
-    />
-  )
+  const component = Builder.shallow({ maxValue: 5 })
   expect(component.find('.rollex-digit-lane-label').length).toBe(7)
 })
---
+++ test/support/builders.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import CounterSegment from '../../src/CounterSegment'
+import StaticCounterDigit from '../../src/StaticCounterDigit'
+import AnimatedCounterDigit from '../../src/AnimatedCounterDigit'
+
+class AbstractBuilder {
+  static shallow = function (params = {}) {
+    return shallow(this.build(params))
+  }
+
+  static mount = function (params = {}) {
+    return mount(this.build(params))
+  }
+}
+
+export class CounterSegmentBuilder extends AbstractBuilder {
+  static build = function ({
+    digits = ['0', '0'],
+    period = 'seconds',
+    radix = 10,
+    easingFunction = null,
+    easingDuration = 300,
+    digitMap = {},
+    digitWrapper = (digit) => <span>{digit}</span>
+  } = {}) {
+    return React.createElement(
+      CounterSegment,
+      { digits, period, radix, easingFunction, easingDuration, digitMap, digitWrapper }
+    )
+  }
+}
+
+export class StaticCounterDigitBuilder extends AbstractBuilder {
+  static build = function ({
+    digit = '0',
+    height = 18,
+    radix = 10,
+    digitMap = {},
+    digitWrapper = (digit) => <span>{digit}</span>
+  } = {}) {
+    return React.createElement(
+      StaticCounterDigit,
+      { digit, height, radix, digitMap, digitWrapper }
+    )
+  }
+}
+
+export class AnimatedCounterDigitBuilder extends AbstractBuilder {
+  static build = function ({
+    digit = '0',
+    height = 18,
+    radix = 10,
+    digitMap = {},
+    digitWrapper = (digit) => <span>{digit}</span>,
+    maxValue = 9,
+    easingFunction = 'ease-in',
+    easingDuration = 300
+  } = {}) {
+    return React.createElement(
+      AnimatedCounterDigit,
+      { digit, height, radix, digitMap, digitWrapper, maxValue, easingFunction, easingDuration }
+    )
+  }
+}
