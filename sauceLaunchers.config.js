b1f182e797fcf33480d2a0b93e6ee8ff27ca2123
Alexander Komarov <ak@akxcv.com>
2017-03-17 22:16:30 +0300

    Add more browsers to Sauce Labs
    Add mobile browsers
    
    add 100ms to animated test
    
    bump to 300ms
    
    Increase timeouts to 5 minutes for mobile emulators
    
    finish

--- sauceLaunchers.config.js
+++ sauceLaunchers.config.js
@@ -1,23 +1,29 @@
 module.exports = {
-  sl_chrome_old: {
+  sl_chrome_26: {
     base: 'SauceLabs',
     browserName: 'chrome',
-    version: '30'
+    version: '26'
   },
-  sl_chrome_new: {
+  sl_chrome_56: {
     base: 'SauceLabs',
     browserName: 'chrome',
     version: '56'
   },
-  sl_firefox_old: {
+  sl_firefox_16: {
     base: 'SauceLabs',
     browserName: 'firefox',
-    version: '30'
+    version: '16'
   },
-  sl_firefox_new: {
+  sl_firefox_51: {
     base: 'SauceLabs',
     browserName: 'firefox',
     version: '51'
+  },
+  sl_ie_10: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    platform: 'Windows 7',
+    version: '10'
   },
   sl_ie_11: {
     base: 'SauceLabs',
@@ -25,16 +31,52 @@ module.exports = {
     platform: 'Windows 8.1',
     version: '11'
   },
-  sl_edge: {
+  sl_edge_13: {
+    base: 'SauceLabs',
+    browserName: 'MicrosoftEdge',
+    platform: 'Windows 10',
+    version: '13.10586'
+  },
+  sl_edge_14: {
     base: 'SauceLabs',
     browserName: 'MicrosoftEdge',
     platform: 'Windows 10',
     version: '14.14393'
+  },
+  sl_safari_7: {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    platform: 'OS X 10.9',
+    version: '7.0'
   },
   sl_safari_10: {
     base: 'SauceLabs',
     browserName: 'safari',
     platform: 'macOS 10.12',
     version: '10.0'
+  },
+  sl_iphone_4s: {
+    base: 'SauceLabs',
+    browserName: 'Safari',
+    platformName: 'iOS',
+    platformVersion: '8.1',
+    deviceName: 'iPhone 4s Simulator',
+    appiumVersion: '1.6.3'
+  },
+  sl_iphone_7: {
+    base: 'SauceLabs',
+    browserName: 'Safari',
+    platformName: 'iOS',
+    platformVersion: '10.2',
+    deviceName: 'iPhone 7 Plus Simulator',
+    appiumVersion: '1.6.3'
+  },
+  sl_android_5_1: {
+    base: 'SauceLabs',
+    browserName: 'Browser',
+    platformName: 'Android',
+    platformVersion: '5.1',
+    deviceName: 'Android Emulator',
+    appiumVersion: '1.5.3'
   }
 }
