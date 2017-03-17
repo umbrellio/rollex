79c0d8c071b940cfd8799f511a56d1086e6aceeb
Alexander Komarov <ak@akxcv.com>
2017-03-17 06:44:37 +0300

    Sauce Launchers config; lower timeout for CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,64 +1,27 @@
 const webpack = require('webpack')
+const sauceLaunchers = require('./sauceLaunchers.config')
 
-const sauceLaunchers = {
-  sl_chrome_old: {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    version: '30'
-  },
-  sl_chrome_new: {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    version: '56'
-  },
-  sl_firefox_old: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    version: '30'
-  },
-  sl_firefox_new: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    version: '51'
-  },
-  sl_ie_11: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 8.1',
-    version: '11'
-  },
-  sl_edge: {
-    base: 'SauceLabs',
-    browserName: 'MicrosoftEdge',
-    platform: 'Windows 10',
-    version: '14.14393'
-  },
-  sl_safari_10: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'macOS 10.12',
-    version: '10.0'
-  }
-}
-
+var browsers
 if (process.env.TRAVIS) {
   // Travis only comes with Firefox
-  var browsers = ['Firefox']
+  browsers = ['Firefox']
 } else if (process.env.SAUCE) {
   if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
     console.error('SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables must be set!')
     process.exit(1)
   }
-  var browsers = Object.keys(sauceLaunchers)
+  browsers = Object.keys(sauceLaunchers)
 } else if (process.platform === 'darwin') {
-  var browsers = ['Chrome', 'Firefox', 'Safari']
+  browsers = ['Chrome', 'Firefox', 'Safari']
 } else {
-  var browsers = ['Chrome', 'Firefox']
+  browsers = ['Chrome', 'Firefox']
 }
 
 module.exports = function (config) {
+  const ci = process.env.CI
+  const sauce = process.env.SAUCE
+
   config.set({
-    browsers: browsers,
     singleRun: true,
     frameworks: ['jasmine'],
     files: [
@@ -92,11 +55,12 @@ module.exports = function (config) {
       stats: 'errors-only'
     },
     sauceLabs: {
-      testName: process.env.CI ? 'Rollex acceptance tests (CI)' : 'Rollex acceptance tests'
+      testName: ci ? 'Rollex acceptance tests (CI)' : 'Rollex acceptance tests'
     },
-    concurrency: 2,
-    captureTimeout: 300000,
-    browserNoActivityTimeout: 300000,
+    browsers: browsers,
+    concurrency: (ci || sauce) ? 2 : Infinity,
+    captureTimeout: ci ? 120000 : 300000,
+    browserNoActivityTimeout: ci ? 120000 : 300000,
     customLaunchers: sauceLaunchers
   })
 }
---
+++ sauceLaunchers.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  sl_chrome_old: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    version: '30'
+  },
+  sl_chrome_new: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    version: '56'
+  },
+  sl_firefox_old: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    version: '30'
+  },
+  sl_firefox_new: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    version: '51'
+  },
+  sl_ie_11: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    platform: 'Windows 8.1',
+    version: '11'
+  },
+  sl_edge: {
+    base: 'SauceLabs',
+    browserName: 'MicrosoftEdge',
+    platform: 'Windows 10',
+    version: '14.14393'
+  },
+  sl_safari_10: {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    platform: 'macOS 10.12',
+    version: '10.0'
+  }
+}
